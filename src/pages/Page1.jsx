93fd4796fcde7387823eed9df8e0a4f047587be1
Maksim <maksimtsoy1990@mail.ru>
2023-05-01 20:27:21 +0500

    start

---
+++ src/pages/Page1.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Page1 = ({
+  title,
+}) => {
+  return (
+    <div style={{ fontSize: '36px', display: 'flex', justifyContent: 'center' }}>
+      {title.attributes.title}
+    </div>
+  )
+};
+
+export default Page1;
